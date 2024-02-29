be2571f5ca90030892deb3fb4119adffead11890
Pedro Barão <81188809+PedroBittarBarao@users.noreply.github.com>
2024-02-29 11:14:08 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import socket
+import time
+import pandas as pd
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+target = input('Where to scan?: ')
+port_start = int(input('Start port: '))
+port_end = int(input('End port: '))
+
+ports = pd.read_csv('tcp.csv', index_col=0) # https://github.com/maraisr/ports-list/blob/main/tcp.csv?plain=1
+ports_dict = ports.set_index('port')['description'].to_dict()
+
+target_ip = socket.gethostbyname(target)
+print('Starting scan on host:', target_ip)
+
+def port_scan(port):
+    try:
+        s.connect((target_ip, port))
+        return True
+    except:
+        return False
+ 
+ 
+start = time.time()
+ 
+# here we are scanning port 0 to 4
+for port in range(port_start, port_end+1):
+    if port_scan(port):
+        print(f'port {port} is open ({ports_dict.get(port, "unknown")})')
+    else:
+        print(f'port {port} is closed ({ports_dict.get(port, "unknown")})')
+ 
+end = time.time()
+print(f'Time taken {end-start:.2f} seconds')
